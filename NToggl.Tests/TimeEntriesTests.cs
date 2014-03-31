c19d53926c9e6a4eac20c050c80dcc034b4796a5
Ali Sabzevari <ali@tidm.ir>
2014-03-31 16:49:22 +0430

    Added TimeEntriesTests

---
+++ NToggl.Tests/TimeEntriesTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NToggl.Tests
+{
+    [TestClass]
+    public class TimeEntriesTests
+    {
+        private readonly string _userAgent;
+        private readonly Client _client;
+
+
+        public TimeEntriesTests()
+        {
+            string apiToken = ConfigurationManager.AppSettings["ApiToken"];
+            _userAgent = ConfigurationManager.AppSettings["UserAgent"];
+            _client = new Client(apiToken);
+        }
+
+        //[TestMethod]
+    }
+}
