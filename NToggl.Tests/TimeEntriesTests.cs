6ccdf85e37b83083f5bc3d2be138698d449c3972
Ali Sabzevari <ali@tidm.ir>
2014-03-31 17:43:45 +0430

    Write TimeEntries Tests

--- NToggl.Tests/TimeEntriesTests.cs
+++ NToggl.Tests/TimeEntriesTests.cs
@@ -18,6 +18,59 @@ namespace NToggl.Tests
             _client = new Client(apiToken);
         }
 
-        //[TestMethod]
+        [TestMethod]
+        public void Get_All_Time_Entries()
+        {
+            var timeEntries = _client.GetTimeEntries();
+            Assert.IsTrue(timeEntries.Any());
+        }
+
+        [TestMethod]
+        public void Get_Time_Entries_By_Time_Filter_Start_Date()
+        {
+            var timeEntries = _client.GetTimeEntries(startDate: DateTime.Now.AddYears(-1));
+            Assert.IsTrue(timeEntries.Any());
+        }
+
+        [TestMethod]
+        public void Get_Time_Entries_By_Time_Filter_End_Date()
+        {
+            var timeEntries = _client.GetTimeEntries(endDate: DateTime.Now);
+            Assert.IsTrue(timeEntries.Any());
+        }
+
+        [TestMethod]
+        public void Get_Time_Entries_By_Time_Filter_Start_Date_And_End_Date()
+        {
+            var timeEntries = _client.GetTimeEntries(startDate: DateTime.Now.AddYears(-1), endDate: DateTime.Now);
+            Assert.IsTrue(timeEntries.Any());
+        }
+
+        [TestMethod]
+        public void Get_Time_Entries_Of_Specific_User()
+        {
+            var timeEntries = _client.GetTimeEntries(user: _userAgent);
+            Assert.IsTrue(timeEntries.Any());
+        }
+
+        [TestMethod]
+        public void Get_Time_Entries_Of_Specific_User_In_Specific_Time()
+        {
+            var timeEntries = _client.GetTimeEntries(user: _userAgent, startDate: DateTime.Now.AddYears(-1), endDate: DateTime.Now);
+            Assert.IsTrue(timeEntries.Any());
+        }
+
+        [TestMethod]
+        public void Query_Over_Time_Entries_For_Specific_User()
+        {
+            var timeEntries = _client.Query<TimeEntry>().Where(te => te.User == _userAgent);
+            Assert.IsTrue(timeEntries.Any());
+        }
+        [TestMethod]
+        public void Query_Over_Time_Entries_For_Specific_Time_Range()
+        {
+            var timeEntries = _client.Query<TimeEntry>().Where(te => te.End < DateTime.Now && te.Start > DateTime.Now.AddYears(-1));
+            Assert.IsTrue(timeEntries.Any());
+        }
     }
 }
