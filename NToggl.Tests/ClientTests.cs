b536a7ca2eee3c3389aa4679b44e26036e6d4bd0
unknown <sabzevari@Sabzevari.tidm.tosanltd.com>
2014-03-31 12:48:31 +0430

    Initial Commit

---
+++ NToggl.Tests/ClientTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NToggl.Tests
+{
+    [TestClass]
+    public class ClientTests
+    {
+        private readonly string _userAgent;
+        private readonly Client _client;
+        public ClientTests()
+        {
+            string apiToken = ConfigurationManager.AppSettings["ApiToken"];
+            _userAgent = ConfigurationManager.AppSettings["UserAgent"];
+            _client = new Client(apiToken);
+        }
+
+        [TestMethod]
+        public void Get_All_Workspaces()
+        {
+            var workspaces = _client.GetWorkspaces();
+            Assert.IsTrue(workspaces.Any());
+        }
+
+        [TestMethod]
+        public void Get_Full_Detailed_Report_For_First_Workspace()
+        {
+            var workspace = _client.GetWorkspaces().FirstOrDefault();
+            Assert.IsNotNull(workspace);
+            var timeEntries = _client.GetDetailedReport(_userAgent, workspace.Id);
+            Assert.IsTrue(timeEntries.Any());
+        }
+
+        [TestMethod]
+        public void Get_Detailed_Report_For_First_Workspace_By_Time_Filter_Since()
+        {
+            var workspace = _client.GetWorkspaces().FirstOrDefault();
+            Assert.IsNotNull(workspace);
+            var timeEntries = _client.GetDetailedReport(_userAgent, workspace.Id, since: DateTime.Now.AddYears(-1));
+            Assert.IsTrue(timeEntries.Any());
+        }
+
+        [TestMethod]
+        public void Get_Detailed_Report_For_First_Workspace_By_Time_Filter_Until()
+        {
+            var workspace = _client.GetWorkspaces().FirstOrDefault();
+            Assert.IsNotNull(workspace);
+            var timeEntries = _client.GetDetailedReport(_userAgent, workspace.Id, until: DateTime.Now);
+            Assert.IsTrue(timeEntries.Any());
+        }
+
+        [TestMethod]
+        public void Get_Detailed_Report_For_First_Workspace_By_Time_Filter_Since_And_Until()
+        {
+            var workspace = _client.GetWorkspaces().FirstOrDefault();
+            Assert.IsNotNull(workspace);
+            var timeEntries = _client.GetDetailedReport(_userAgent, workspace.Id, since: DateTime.Now.AddYears(-1), until: DateTime.Now);
+            Assert.IsTrue(timeEntries.Any());
+        }
+    }
+}
---
+++ NToggl/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NToggl
+{
+    public class Client
+    {
+        private string _apiToken;
+        private string _url;
+
+        private string WorkspacesAddress
+        {
+            get { return _url + "v8/workspaces/"; }
+        }
+
+        public Client(string apiToken, string url = "https://www.toggl.com/api/")
+        {
+            _apiToken = apiToken;
+            _url = url;
+        }
+
+        public IEnumerable<Workspace> GetWorkspaces()
+        {
+            
+        }
+    }
+}
