dac20a35eb1e8e2c1e15fe773e787ea96b1a53fe
Hamid Jahtalab <hamid@tidm.ir>
2014-03-31 17:30:15 +0430

    Add: ProjectTests & ProjectUserTests

---
+++ NToggl.Tests/ProjectTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NToggl.Tests
+{
+    [TestClass]
+    public class ProjectTests
+    {
+        private readonly string _userAgent;
+        private readonly Client _client;
+        public ProjectTests()
+        {
+            string apiToken = ConfigurationManager.AppSettings["ApiToken"];
+            _userAgent = ConfigurationManager.AppSettings["UserAgent"];
+            _client = new Client(apiToken);
+        }
+
+        [TestMethod]
+        public void Create_Project()
+        {
+            var project = _client.CreateProject(new Project()
+            {
+                Name = "ProjectName",
+                Wid = 1,
+                TemplateId = 1,
+                IsPrivate = true,
+            });
+            Assert.AreEqual(project.Name, "ProjectName");
+        }
+        [TestMethod]
+        public void Get_Project()
+        {
+            var project = _client.GetProject(1);
+            Assert.AreEqual(project.Name == "ProjectName");
+        }
+        [TestMethod]
+        public void Update_Project()
+        {
+            var project = _client.GetProject(1);
+            project.Name = "ChangedName";
+            var result = _client.UpdateProject(project);
+            Assert.AreEqual(result.Name == "ChangedName");
+        }
+        [TestMethod]
+        public void Delete_Project()
+        {
+            _client.DeleteProject(1);
+        }
+        [TestMethod]
+        public void Get_Project_Users()
+        {
+            var users = _client.GetProjectUsers(1);
+            Assert.IsTrue(users.Any());
+
+        }
+    }
+}
---
+++ NToggl.Tests/ProjectUserTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NToggl.Tests
+{
+    [TestClass]
+    public class ProjectUserTests
+    {
+        private readonly string _userAgent;
+        private readonly Client _client;
+        public ProjectUserTests()
+        {
+            string apiToken = ConfigurationManager.AppSettings["ApiToken"];
+            _userAgent = ConfigurationManager.AppSettings["UserAgent"];
+            _client = new Client(apiToken);
+        }
+        [TestMethod]
+        public void Create_Project_User()
+        {
+            var project = _client.GetProject(1);
+            var user = _client.GetUser(1);
+            var result = _client.CreateProjectUser(new User()
+            {
+                Project = project,
+                User = user,
+                Rate = 0.4,
+                IsManager = true
+            });
+            Assert.AreNotEqual(result.Id, 0);
+        }
+        [TestMethod]
+        public void Update_Project_User()
+        {
+            var projectUser = _client.GetProjectUser(1);
+            projectUser.Rate = 15;
+            _client.UpdateProjectUser(projectUser);
+            Assert.AreEqual(projectUser.Rate, 15);
+        }
+        [TestMethod]
+        public void Delete_Project_User()
+        {
+            _client.DeleteProjectUser(1);
+        }
+
+    }
+}
